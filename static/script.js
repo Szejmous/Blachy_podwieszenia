820c5308e5a6a423f6b67c292563e727a6649f15
Szejmous <arekstachyra97@gmail.com>
2025-03-14 18:14:41 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -85,8 +85,8 @@ async function calculate() {
     resultSpan.style.fontWeight = "bold";
 
     // Aktualizacja nagłówka z obciążeniem ciągłym
-    document.getElementById("uniform-load-heading").innerText = `Fałda obciążona obciążeniem ciągłym (q = ${result.load_kg_m.toFixed(2)} kg/m)`;
-    document.getElementById("uniform-load-heading").style.fontSize = "16px"; // Domyślna wielkość h2
+    document.getElementById("uniform-load-heading").innerText = `Pojedyncza fałda - maksymalne obciążenie wzdłuż równomiernie (nie punktowe) (q = ${result.load_kg_m.toFixed(2)} kg/m)`;
+    document.getElementById("uniform-load-heading").style.fontSize = "16px"; // Taka sama jak h2
     document.getElementById("uniform-load-heading").style.fontWeight = "normal";
 
     // Usuń istniejące span-y, aby uniknąć duplikatów
